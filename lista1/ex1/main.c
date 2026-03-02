d8d2945ff20973eb069663eb83f5a774b44d5210
thiagopls1 <thiagopls1@hotmail.com>
2026-03-02 11:08:17 -0300

    feat: add `lista1/ex1` thread creation

--- lista1/ex1/main.c
+++ lista1/ex1/main.c
@@ -1,6 +1,48 @@
+#include <pthread.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+const unsigned int THREADS_COUNT = 2;
+
+typedef struct {
+
+} workerParams;
+
+void *pairWorker(void *arg) {
+  // workerParams *params = (workerParams *)arg;
+
+  return NULL;
+}
 
 int main() {
-  printf("Hello, World!");
+  pthread_mutex_t mutex;
+
+  pthread_cond_t man_arrived;
+  pthread_cond_t woman_arrived;
+
+  // INIT
+  pthread_mutex_init(&mutex, NULL);
+
+  pthread_cond_init(&man_arrived, NULL);
+  pthread_cond_init(&woman_arrived, NULL);
+
+  // Threads Creation
+  pthread_t threads[THREADS_COUNT];
+
+  for (int i = 0; i < THREADS_COUNT; i++)
+    pthread_create(&threads[i], NULL, pairWorker, NULL);
+
+  for (int i = 0; i < THREADS_COUNT; i++)
+    pthread_join(threads[i], NULL);
+
+  // DESTROY
+  pthread_mutex_destroy(&mutex);
+
+  pthread_cond_destroy(&man_arrived);
+  pthread_cond_destroy(&woman_arrived);
+
   return 0;
 }
